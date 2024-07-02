9b9faa8af291460a6214392ed57ca14d0d55e8d9
Ruthvik L Bhat <ruthvik.bhat@xtendr.co>
2024-07-02 19:53:23 +0530

    remove static generation

--- next.config.js
+++ next.config.js
@@ -12,7 +12,6 @@ const withPWA = require('@ducanh2912/next-pwa').default({
 })
 
 const nextConfig = {
-  output: 'export',
   // uncomment the following snippet if using styled components
   // compiler: {
   //   styledComponents: true,
