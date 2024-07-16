1668dd2208c6a22102b1e93a9ced2c082f91e379
Ruthvik L Bhat <ruthvik.bhat@xtendr.co>
2024-07-17 00:18:22 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -10,13 +10,34 @@ export default function Page() {
   const [ended, setEnded] = useState(false);
   const [showVideo, setShowVideo] = useState(false);
 
+  // Check for previous visits outside useEffect
+  const hasVisited = typeof window !== 'undefined' && sessionStorage.getItem('hasVisited');
+
   useEffect(() => {
-    const hasVisited = sessionStorage.getItem('hasVisited');
+    const trackPageView = () => {
+      if (typeof window.ttq !== 'undefined') {
+        console.log('Tracking page view');
+        window.ttq.track("ViewContent", {
+          contents: [
+            {
+              content_id: "hyundai-configurator-viewcontent",
+              content_name: "Hyundai Configurator Home Page Load",
+              content_type: "product",
+              content_category: "3d configurator",
+              quantity: 1
+            },
+          ],
+          currency: "USD"
+        });
+      }
+    };
+
     if (!hasVisited) {
       setShowVideo(true);
       sessionStorage.setItem('hasVisited', 'true');
     }
-  }, []);
+    setTimeout(trackPageView, 500);
+  }, [hasVisited]);
 
   return (
     <div className='mt-2 flex h-full flex-col justify-evenly overflow-y-scroll'>
