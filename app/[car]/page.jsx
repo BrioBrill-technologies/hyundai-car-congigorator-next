f6f3ff1156e96a020b6ed347545af497608752e3
Balakrishna K <kbalakrishna017@gmail.com>
2024-07-25 19:47:02 +0530

    video bugs fixed

--- app/[car]/page.jsx
+++ app/[car]/page.jsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useEffect, useRef } from 'react'
 import { useRouter } from 'next/navigation'
 import { cars } from '@/data/cars'
 
@@ -8,71 +9,100 @@ export default function Page({ params }) {
   car = decodeURIComponent(car)
   const router = useRouter()
 
+  const videoRefs = useRef([])
+
   const handleSelectClick = (trim) => {
-    window.ttq.track("ClickButton",
-      {
-        contents: [
-          {
-            content_id: `${car}-${trim}-trim-selection`, //Dynamic value reflecting user selection
-            content_name: `${car} ${trim} Trim Selection`, //Dynamic value reflecting user selection
-            content_type: "product", //Hard coded
-            content_category: "3d configurator", //Hard coded
-            quantity: 1, //Hard coded
-            price: 50000, //Dynamic value reflecting user selection
-          },
-        ],
-        value: 50000, //Dynamic value reflecting user selection
-        currency: "USD",
-      }
-    );
+    window.ttq.track("ClickButton", {
+      contents: [
+        {
+          content_id: `${car}-${trim}-trim-selection`,
+          content_name: `${car} ${trim} Trim Selection`,
+          content_type: "product",
+          content_category: "3d configurator",
+          quantity: 1,
+          price: 50000,
+        },
+      ],
+      value: 50000,
+      currency: "USD",
+    })
     router.push(`/${car}/${trim}/`)
   }
+
+  const setPlaybackRate = (rate) => {
+    if (videoRefs.current.length > 0) {
+      videoRefs.current.forEach((video) => {
+        if (video) {
+          video.playbackRate = rate
+        }
+      })
+    }
+  }
+
+  const synchronizeVideos = () => {
+    if (videoRefs.current.length > 0) {
+      videoRefs.current.forEach((video) => {
+        if (video) {
+          video.currentTime = 0
+          video.play()
+        }
+      })
+      setPlaybackRate(0.7) // Slow down the videos to half speed
+    }
+  }
+
+  useEffect(() => {
+    synchronizeVideos()
+  }, [])
 
   return (
     <div className='mx-auto flex size-full flex-col gap-2 overflow-y-scroll'>
       <h1 className='text-center text-3xl'>{car === 'IONIQ5' ? 'IONIQ 5' : 'IONIQ 6'}</h1>
       <p className='text-center font-[HyundaiSansHead-Medium]'>Choose a Trim</p>
       <div className='flex h-fit w-full flex-row justify-evenly gap-5 overflow-x-scroll px-5'>
-        {Object.keys(cars[car]).map((trim) => {
-          // return only if trim is image
+        {Object.keys(cars[car]).map((trim, index) => {
           if (trim === 'image') {
             return null
           }
           return (
             <div key={trim} className='h-fit'>
-              {trim === 'image' ? null : (
-                <div className='mx-auto mt-5 h-fit min-w-72 cursor-pointer items-center rounded-lg bg-gradient-to-br from-gray-200/40
-                  to-transparent bg-clip-padding py-5 text-black backdrop-blur-sm'>
-                  <img
-                    className='size-full rounded-lg object-cover'
-                    src={`/${car}/${trim}.gif`}
-                    alt={`${trim} trim`}
-                    preload='auto'
-                  />
-                  <div>
-                    <p className='mt-5 text-center'>{trim === 'D100PlatinumEdition' ? 'D100 Platinum Edition' : trim}</p>
-                    <p className='text-center font-[HyundaiSansHead-Light] text-xs'>{cars[car][trim].description}</p>
-                  </div>
-                  <div
-                    className={`w-full border-2 border-black py-2 text-center ${trim === 'Limited' ? 'mt-1' : 'mt-5'} cursor-pointer font-[HyundaiSansHead-Regular]`}
-                    onClick={() => handleSelectClick(trim)}
-                  >
-                    Select {trim === 'D100PlatinumEdition' ? 'D100 Platinum Edition' : trim}
-                    <span className='absolute right-5'>
-                      <svg
-                        xmlns='http://www.w3.org/2000/svg'
-                        fill='none'
-                        viewBox='0 0 24 24'
-                        strokeWidth='2.5'
-                        stroke='currentColor'
-                        className='size-6'
-                      >
-                        <path strokeLinecap='round' strokeLinejoin='round' d='m8.25 4.5 7.5 7.5-7.5 7.5' />
-                      </svg>
-                    </span>
-                  </div>
+              <div className='mx-auto mt-5 h-fit min-w-72 cursor-pointer items-center rounded-lg bg-gradient-to-br from-gray-200/40
+                to-transparent bg-clip-padding py-5 text-black backdrop-blur-sm'>
+                <video
+                  ref={(el) => (videoRefs.current[index] = el)}
+                  className='size-full rounded-lg object-cover'
+                  autoPlay
+                  loop
+                  muted
+                  playsInline
+                  type='video/mp4'
+                  src={`/${car}/${trim}.mp4`}
+                  alt={`${trim} trim`}
+                  preload='auto'
+                />
+                <div>
+                  <p className='mt-5 text-center'>{trim === 'D100PlatinumEdition' ? 'D100 Platinum Edition' : trim}</p>
+                  <p className='text-center font-[HyundaiSansHead-Light] text-xs'>{cars[car][trim].description}</p>
                 </div>
-              )}
+                <div
+                  className={`w-full border-2 border-black py-2 text-center ${trim === 'Limited' ? 'mt-1' : 'mt-5'} cursor-pointer font-[HyundaiSansHead-Regular}`}
+                  onClick={() => handleSelectClick(trim)}
+                >
+                  Select {trim === 'D100PlatinumEdition' ? 'D100 Platinum Edition' : trim}
+                  <span className='absolute right-5'>
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      fill='none'
+                      viewBox='0 0 24 24'
+                      strokeWidth='2.5'
+                      stroke='currentColor'
+                      className='size-6'
+                    >
+                      <path strokeLinecap='round' strokeLinejoin='round' d='m8.25 4.5 7.5 7.5-7.5 7.5' />
+                    </svg>
+                  </span>
+                </div>
+              </div>
             </div>
           )
         })}
