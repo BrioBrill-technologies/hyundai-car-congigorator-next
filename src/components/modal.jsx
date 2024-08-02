719e01c65fb7fa7d6545e913a3ca39c13eafa5ec
Balakrishna K <kbalakrishna017@gmail.com>
2024-08-03 01:11:12 +0530

    Update modal.jsx

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -31,7 +31,7 @@ export const Modal = ({ visible, setVisibility, title, description }) => {
       aria-labelledby="modal-title"
       aria-describedby="modal-description"
     >
-      <div className="pointer-events-none mx-auto flex w-9/12 flex-col justify-center gap-2 rounded-lg bg-black/35 p-2 text-center text-white">
+      <div className="pointer-events-none mx-auto flex w-4/5 flex-col justify-center gap-2 rounded-lg bg-black/35 p-2 text-center text-white">
         <div className="flex flex-col gap-2">
           <p className="text-sm" id="modal-title">{title}</p>
           <p className="text-xs" id="modal-description">{description}</p>
